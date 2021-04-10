8c46282a936b1b382ca491f2f9e5293aa885bca7
Eduardo Mendieta <mendieta.eduu@gmail.com>
2021-04-10 18:00:54 -0500

    creando la estructura de ficheros

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuPrincipalComponent } from './components/shared/menu-principal/menu-principal.component';
+import { RegistroUsuariosComponent } from './components/pages/usuarios/registro-usuarios/registro-usuarios.component';
+import { ConfiguracionCuentaComponent } from './components/pages/usuarios/configuracion-cuenta/configuracion-cuenta.component';
+import { RegistroRestaurantesComponent } from './components/pages/restaurantes/registro-restaurantes/registro-restaurantes.component';
+import { CatalogoRestaurantesComponent } from './components/pages/restaurantes/catalogo-restaurantes/catalogo-restaurantes.component';
+import { CatalogoPlatosComponent } from './components/pages/platos/catalogo-platos/catalogo-platos.component';
+import { EventosComponent } from './components/pages/extras/eventos/eventos.component';
+import { LoginComponent } from './components/shared/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuPrincipalComponent,
+    RegistroUsuariosComponent,
+    ConfiguracionCuentaComponent,
+    RegistroRestaurantesComponent,
+    CatalogoRestaurantesComponent,
+    CatalogoPlatosComponent,
+    EventosComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/pages/restaurantes/registro-restaurantes/registro-restaurantes.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-registro-restaurantes',
+  templateUrl: './registro-restaurantes.component.html',
+  styleUrls: ['./registro-restaurantes.component.css']
+})
+export class RegistroRestaurantesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
